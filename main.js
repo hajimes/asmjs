b0645f2c359602f5c3d38ea30f018b730d949ec1
Hajime Senuma <hajime.senuma@gmail.com>
2016-05-26 00:47:17 +0900

    Add utf8-to-utf16

--- main.js
+++ main.js
@@ -540,15 +540,47 @@
       return h1 | 0;
     }
     
+    
+    /**
+     * @parma {number} b - first byte of a utf-8 sequence
+     * @returns {number} - number of trailing bytes for the sequence
+     */
+    function uc_trailingBytesForUtf8(b) {
+      /*
+       * Type annotations
+       */
+      b = b | 0;
+      
+      /*
+       * Main
+       */   
+      b = b & 0xff;
+      if ((b | 0) < 192) {
+        return 0;
+      } else if ((b | 0) < 224) {
+        return 1;
+      } else if ((b | 0) < 240) {
+        return 2;
+      } else if ((b | 0) < 248){
+        return 3;
+      }
+      
+      
+      // invalid
+      return 0xff;
+    }
+    
     /**
      * Based on ConvertUTF.c by Unicode, Inc.
+     * Endian dependent.
      *
      * @param {number} inPP - byte offset to a byte offset to uint16s
      * @param {number} inEnd - byte offset to the end of inputs
      * @param {number} outPP - byte offset to a byte offset to uint8s
      * @param {number} outEnd - byte offset to the end of outputs
+     * @returns {number} - error code
      */
-    function convertUtf16toUtf8(inPP, inEnd, outPP, outEnd) {
+    function uc_convertUtf16toUtf8(inPP, inEnd, outPP, outEnd) {
       /*
        * Type annotations
        */
@@ -668,6 +700,140 @@
       
       return 0;
     }
+    
+    /**
+     * Based on ConvertUTF.c by Unicode, Inc.
+     * Endian dependent.
+     *
+     * @param {number} inPP - byte offset to a byte offset to uint16s
+     * @param {number} inEnd - byte offset to the end of inputs
+     * @param {number} outPP - byte offset to a byte offset to uint8s
+     * @param {number} outEnd - byte offset to the end of outputs
+     * @returns {number} - error code
+     */
+    function uc_convertUtf8toUtf16(inPP, inEnd, outPP, outEnd) {
+      /*
+       * Type annotations
+       */
+      inPP = inPP | 0;
+      inEnd = inEnd | 0;
+      outPP = outPP | 0;
+      outEnd = outEnd | 0;
+      
+      /*
+       * Local variables
+       */
+      var SUR_HIGH_START = 0xd800;
+      var SUR_LOW_START = 0xdc00;
+      var SUR_LOW_END = 0xdfff;
+      var HALF_SHIFT = 10;
+      var HALF_BASE = 0x0010000;
+      var HALF_MASK = 0x3ff;
+      var ERROR_SOURCE_EXHAUSTED = 1;
+      var ERROR_TARGET_EXHAUSTED = 2;
+      var ERROR_SOURCE_ILLEGAL = 3;
+      var result = 0;
+      var ch = 0;
+      var v = 0;
+      var inP = 0;
+      var outP = 0;
+      var extraBytesToRead = 0;
+      
+      /*
+       * Main
+       */
+      inP = u32heap[inPP >> 2] | 0;
+      outP = u32heap[outPP >> 2] | 0;  
+      while ((inP | 0) < (inEnd | 0)) {
+        ch = 0;
+        v = u8heap[inP >> 0] | 0;
+        extraBytesToRead = uc_trailingBytesForUtf8(v) | 0;
+        if ((extraBytesToRead | 0) >= ((inEnd - inP) | 0)) {
+          result = ERROR_SOURCE_EXHAUSTED | 0;
+          break;
+        }
+        
+        // if (!isLegalUtf8)
+                
+        switch (extraBytesToRead | 0) {
+          case 3:
+            v = u8heap[inP >> 0] | 0;
+            ch = (ch + v) | 0;
+            inP = (inP + 1) | 0;
+            ch = ch << 6;
+            // fall through
+          case 2:
+            v = u8heap[inP >> 0] | 0;
+            ch = (ch + v) | 0;
+            inP = (inP + 1) | 0;
+            ch = ch << 6;
+            // fall through
+          case 1:
+            v = u8heap[inP >> 0] | 0;
+            ch = (ch + v) | 0;
+            inP = (inP + 1) | 0;
+            ch = ch << 6;
+            // fall through
+          case 0:
+            v = u8heap[inP >> 0] | 0;
+            ch = (ch + v) | 0;
+            inP = (inP + 1) | 0;
+        }
+        
+        switch (extraBytesToRead | 0) {
+          case 3:
+            ch = (ch - 0x3c82080) | 0;
+            break;
+          case 2:
+            ch = (ch - 0xe2080) | 0;
+            break;
+          case 1:
+            ch = (ch - 0x3080) | 0;
+            break;
+        }
+
+        if ((outP | 0) >= (outEnd | 0)) {
+          inP = (inP - extraBytesToRead + 1) | 0;
+          result = ERROR_TARGET_EXHAUSTED;
+          break;
+        }
+        
+        if ((ch | 0) <= 0xffff) {
+          // if BMP
+          if (((ch | 0) >= (SUR_HIGH_START | 0)) &
+            ((ch | 0) <= (SUR_LOW_END | 0))) {
+            inP = (inP - extraBytesToRead + 1) | 0;
+            result = ERROR_SOURCE_ILLEGAL = 3;
+            break;
+          } else {
+            u16heap[outP >> 1] = ch | 0;
+            outP = (outP + 2) | 0;
+          }
+        } else if ((ch | 0) > 0x10ffff) {
+          // if outside Unicode
+          result = ERROR_SOURCE_ILLEGAL | 0;
+          inP = (inP - extraBytesToRead + 1) | 0;
+          break;
+        } else {
+          // if non-BMP
+          if ((outP | 0) >= (outEnd | 0)) {
+            inP = (inP - extraBytesToRead + 1) | 0;
+            result = ERROR_TARGET_EXHAUSTED;
+            break;
+          }
+          ch = (ch - HALF_BASE) | 0;
+          u16heap[outP >> 1] = ((ch >> HALF_SHIFT) + SUR_HIGH_START) | 0;
+          outP = (outP + 2) | 0;
+          u16heap[outP >> 1] = ((ch & HALF_MASK) + SUR_LOW_START) | 0;
+        }
+        
+      } // end while
+      
+      u32heap[inPP >> 2] = inP | 0;
+      u32heap[outPP >> 2] = outP | 0;
+      
+      return result | 0;
+    }
         
     return {
       ufmap_create: ufmap_create,
@@ -677,7 +843,8 @@
       ufmap_size: ufmap_size,
       maxFloat32: maxFloat32,
       logsumexp: logsumexp,
-      convertUtf16toUtf8: convertUtf16toUtf8,
+      uc_convertUtf16toUtf8: uc_convertUtf16toUtf8,
+      uc_convertUtf8toUtf16: uc_convertUtf8toUtf16,
       hash: hash
     };
   }
